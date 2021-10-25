669c1762805655e3c10efc743a6b59b710e2c929
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-10-25 08:55:40 -0700

    added initial results page with dynamic HTML

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import pokedex from './src/pokemon.js';
+import pokemonList from './src/pokemon.js';
+import * as u from './utils.js';
 
 const poke1Radio = document.getElementById('poke-1-radio');
 const poke2Radio = document.getElementById('poke-2-radio');
@@ -11,28 +12,28 @@ const submitButton = document.getElementById('submit-button');
 // const resetButton = document.getElementById('reset-button');
 
 const generatePokemon = () => {
-    let randNum1 = Math.floor(Math.random() * pokedex.length);
-    let randNum2 = Math.floor(Math.random() * pokedex.length);
-    let randNum3 = Math.floor(Math.random() * pokedex.length);
+    let randNum1 = Math.floor(Math.random() * pokemonList.length);
+    let randNum2 = Math.floor(Math.random() * pokemonList.length);
+    let randNum3 = Math.floor(Math.random() * pokemonList.length);
 
     while (randNum1 === randNum2 || randNum1 === randNum3 || randNum2 === randNum3) {
-        randNum1 = Math.floor(Math.random() * pokedex.length);
-        randNum2 = Math.floor(Math.random() * pokedex.length);
-        randNum3 = Math.floor(Math.random() * pokedex.length);
+        randNum1 = Math.floor(Math.random() * pokemonList.length);
+        randNum2 = Math.floor(Math.random() * pokemonList.length);
+        randNum3 = Math.floor(Math.random() * pokemonList.length);
     }
 
-    let poke1 = pokedex[randNum1];
-    // encountered(poke1.id);
+    let poke1 = pokemonList[randNum1];
+    u.encountered(poke1.id);
     poke1Img.src = poke1.url_image;
     poke1Radio.value = poke1.id;
 
-    let poke2 = pokedex[randNum2];
-    // encountered(poke2.id);
+    let poke2 = pokemonList[randNum2];
+    u.encountered(poke2.id);
     poke2Img.src = poke2.url_image;
     poke2Radio.value = poke2.id;
 
-    let poke3 = pokedex[randNum3];
-    // encountered(poke3.id);
+    let poke3 = pokemonList[randNum3];
+    u.encountered(poke3.id);
     poke3Img.src = poke3.url_image;
     poke3Radio.value = poke3.id;
 };
